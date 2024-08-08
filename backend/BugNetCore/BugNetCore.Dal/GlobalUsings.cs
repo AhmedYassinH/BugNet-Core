714a5b7be44d2c2f39bfe1771574f031baaf259e
AhmedYassin <dev.ahmedyassin@gmail.com>
2024-08-08 10:17:31 +0400

    feat(dal-context): create the main DbContext for the Application
    - Added a DbSet entry for each entity.
    - Added the configurations from the Models project.

---
+++ backend/BugNetCore/BugNetCore.Dal/GlobalUsings.cs
@@ -0,0 +1,21 @@
+﻿
+
+
+// System
+
+
+// Microsoft
+global using Microsoft.EntityFrameworkCore;
+
+
+
+
+// BugNetCore
+// BugNetCore.Dal
+global using BugNetCore.Dal.EFStructures;
+global using Microsoft.EntityFrameworkCore.Storage;
+
+// BugNetCore.Models
+global using BugNetCore.Models.Entities;
+global using BugNetCore.Models.Entities.Base;
+global using BugNetCore.Models.Entities.Configuration;
