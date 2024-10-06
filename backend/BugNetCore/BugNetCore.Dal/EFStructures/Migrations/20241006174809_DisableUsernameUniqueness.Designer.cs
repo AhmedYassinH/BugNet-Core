b05143a098f72a9eb241e64b43c456d1695f455e
AhmedYassin <dev.ahmedyassin@gmail.com>
2024-10-06 21:49:38 +0400

    feat(db_migration): delete db configs that inforce user's username uniqueness

--- backend/BugNetCore/BugNetCore.Dal/EFStructures/Migrations/20241006174809_DisableUsernameUniqueness.Designer.cs
+++ backend/BugNetCore/BugNetCore.Dal/EFStructures/Migrations/20241006174809_DisableUsernameUniqueness.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BugNetCore.Dal.EFStructures;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BugNetCore.Dal.EFStructures.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241006174809_DisableUsernameUniqueness")]
+    partial class DisableUsernameUniqueness
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/BugNetCore/BugNetCore.Dal/EFStructures/Migrations/20241006174809_DisableUsernameUniqueness.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BugNetCore.Dal.EFStructures.Migrations
+{
+    /// <inheritdoc />
+    public partial class DisableUsernameUniqueness : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+    }
+}
